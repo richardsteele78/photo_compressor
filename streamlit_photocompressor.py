02cba4bf2c2f9500000e37b982095e3ad004dcf6
richardsteele78 <82894031+richardsteele78@users.noreply.github.com>
2023-04-06 09:29:47 +0100

    Add scatter plot (draft)

--- streamlit_photocompressor.py
+++ streamlit_photocompressor.py
@@ -1,12 +1,16 @@
-# streamlit run "C:\Users\RichardSteele\Documents\Python Scripts\streamlit_photocompressor.py"
+# streamlit run "C:\Users\RichardSteele\Documents\Python Scripts\Image\streamlit_photocompressor.py"
+
 import numpy as np
 from PIL import Image
 from sklearn.cluster import KMeans
 import streamlit as st
-
-st.header("📷 K-MEANS COMPRESSION")
+import plotly.express as px
+import pandas as pd
+import matplotlib.pyplot as plt
+ 
+st.header("📷 K-MEANS PHOTO COMPRESSION")
 img_file_buffer = st.camera_input("Take a picture")
-num_of_centroids = st.slider(label="Select number of KMEANS CLUSTERS to compress image",min_value=2,max_value=20,value=4)
+num_of_centroids = st.slider(label="Select number of colours (KMEANS CLUSTERS) to compress image",min_value=2,max_value=20,value=4)
 
 if img_file_buffer is not None:
     # To read image file buffer as a PIL Image:
@@ -18,8 +22,13 @@ if img_file_buffer is not None:
     image_width = im.width
     pixel_np = np.reshape(pixel_np, (image_height * image_width, 3))
     # run k-means clustering on the pixel data
-    compressor = KMeans(n_clusters=num_of_centroids)
-    compressor.fit(pixel_np)
+    @st.cache_data
+    def fit_kmeans_model(pixel_np,num_of_centroids):
+        compressor = KMeans(n_clusters=num_of_centroids)
+        compressor.fit(pixel_np)
+        #st.balloons()
+        return compressor
+    compressor = fit_kmeans_model(pixel_np,num_of_centroids)
     # create an array replacing each pixel label with its corresponding cluster centroid
     pixel_centroid = np.array([list(compressor.cluster_centers_[label]) for label in compressor.labels_])
     # convert the array to an unsigned integer type
@@ -27,7 +36,29 @@ if img_file_buffer is not None:
     # reshape this array according to the height and width of our image
     pixel_centroids_reshaped = np.reshape(pixel_centroid, (image_height, image_width, 3), "C")
     # create the compressed image
-    compressed_im = Image.fromarray(pixel_centroids_reshaped)
-    st.image(compressed_im)
+    @st.cache_data
+    def compress_image(pixel_centroids_reshaped):
+        compressed_im = Image.fromarray(pixel_centroids_reshaped)
+        st.image(compressed_im)
+        #st.balloons()
+        return compressed_im
+    compress_image(pixel_centroids_reshaped)
     st.header("RGB Codes of " + str(num_of_centroids) + " colours")
-    st.write(compressor.cluster_centers_)
+    #df['Red'] = compressor.cluster_centers_
+    fulldf = pd.DataFrame(data=pixel_np , columns=['RED','GREEN','BLUE'])
+    df = pd.DataFrame(data=compressor.cluster_centers_ , columns=['RED','GREEN','BLUE'])
+    df['RGBCODE'] = '(' + df["RED"].astype(str) + ',' + df["GREEN"].astype(str) + ',' + df["BLUE"].astype(str) + ')'
+    st.write(df)
+    fig1 = px.scatter_3d(df, x='RED', y='GREEN', z='BLUE')
+    st.plotly_chart(fig1)
+    fullscatter = st.button("Load full pixel scatter? (takes a few seconds)")
+    if fullscatter:
+#        fig2 = px.scatter_3d(fulldf, x='RED', y='GREEN', z='BLUE')
+#        fig2.update_traces(marker_size = 1)
+#        st.plotly_chart(fig2)
+        fig3 = plt.figure()
+        ax = fig3.add_subplot(projection='3d')
+        # Creating plot
+        ax.scatter(xs=fulldf['RED'], ys=fulldf['GREEN'], zs=fulldf['BLUE'],s = 1,alpha=0.01, color = "gray")
+        ax.scatter(xs=df['RED'], ys=df['GREEN'], zs=df['BLUE'],s = 50,alpha=1,c="black") #, color = "green")
+        st.pyplot(fig3)
